20af45e6c41274071cccf09c82b76f3a28530eb9
Josema Arrabal <josemanuelarrabal@facephi.com>
2022-01-24 10:12:51 +0100

    4rd commit: getRetrofit() created in MainActivity

--- app/src/main/java/dev/arrabaljosema/dogsretrofitcoroutines/MainActivity.kt
+++ app/src/main/java/dev/arrabaljosema/dogsretrofitcoroutines/MainActivity.kt
@@ -3,6 +3,8 @@ package dev.arrabaljosema.dogsretrofitcoroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.arrabaljosema.dogsretrofitcoroutines.databinding.ActivityMainBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    /* Se crea instancia del objeto retrofit. Va a tener url original, conversor json a este
+    * modelo de datos y toda la config para hacer llamadas a internet.*/
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breeds/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
