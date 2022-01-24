33205c771e81e8bf07e0395b5a28af9d315581c3
Josema Arrabal <josemanuelarrabal@facephi.com>
2022-01-24 09:54:12 +0100

    2nd commit: Data class created

---
+++ app/src/main/java/dev/arrabaljosema/dogsretrofitcoroutines/DogsResponse.kt
@@ -0,0 +1,9 @@
+package dev.arrabaljosema.dogsretrofitcoroutines
+
+import com.google.gson.annotations.SerializedName
+
+// Con la anotación @SerializedName, se puede cambiar de nombre al parámetro recibido.
+data class DogsResponse(
+    @SerializedName("status") var status: String,
+    @SerializedName("message") var images: List<String>
+)
